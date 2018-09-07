f4a0a9f9053ad3fa821d8a597ceda6e942dcb110
Tsunemichi Harada <fox10225fox@gmail.com>
2018-09-07 23:24:43 +0900

    add contact

---
+++ app/javascript/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => (
+  <div>
+    <h1>Contact</h1>
+    <p>
+      Contact the Ruby on Rails Tutorial about the sample app at the
+      <a href="https://railstutorial.jp/contact">contact page</a>.
+    </p>
+  </div>
+);
+
+export default Contact;
