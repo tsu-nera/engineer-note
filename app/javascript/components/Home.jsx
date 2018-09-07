ad336a3089c8b4f3eb20c6f1c2144abfc9e2cde8
Tsunemichi Harada <fox10225fox@gmail.com>
2018-09-07 15:37:27 +0900

    add eslint

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => (
+  <html>
+    <head>
+      <title>Home | Ruby on Rails Tutorial Sample App</title>
+    </head>
+    <body>
+      <h1>Sample App</h1>
+      <p>
+        This is the home page for the
+        <a href="https://railstutorial.jp/">Ruby on Rails Tutorial</a>
+        sample application.
+      </p>
+    </body>
+  </html>
+);
+
+export default Home;
