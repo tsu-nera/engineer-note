8dca1eddf8af3791b8960aa7361a26169f0d3739
Tsunemichi Harada <fox10225fox@gmail.com>
2018-09-07 17:08:18 +0900

    5.1.1

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
 const Home = () => (
-  <div>
-    <h1>Sample App</h1>
-    <p>
+  <div className="center jumbotron">
+    <h1>Welcome to the Sample App</h1>
+
+    <h2>
       This is the home page for the
       <a href="https://railstutorial.jp/">Ruby on Rails Tutorial</a>
       sample application.
-    </p>
+    </h2>
+    <BrowserRouter>
+      <Link to="#" className="btn btn-lg btn-primary">Sign up now!</Link>
+    </BrowserRouter>
   </div>
 );
 
